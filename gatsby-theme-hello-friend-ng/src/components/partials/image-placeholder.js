3a87da8bd524fdb0711d84eb6348c3cab02fae68
Filip Krawczyk <fkrawczyk77@gmail.com>
2021-04-29 18:29:07 +0100

    Use pure CSS instead of media query hook for image placeholders

--- gatsby-theme-hello-friend-ng/src/components/partials/image-placeholder.js
+++ gatsby-theme-hello-friend-ng/src/components/partials/image-placeholder.js
@@ -1,42 +1,45 @@
-import React, { useState, useEffect, useContext } from "react"
-import MediaQueryContext from "../../context/media-query-context"
+import React from "react"
+// import MediaQueryContext from "../../context/media-query-context"
 
-const getContainerWidth = (mediaQueryMatch, phoneWidth, wide) => {
-  switch (mediaQueryMatch) {
-    case "--fullWidth":
-      return wide ? 860 : 760
-    case "--tabletWidth":
-      return 560
-    case "--phoneWidth":
-      return phoneWidth - 40
-  }
-}
+// const getContainerWidth = (mediaQueryMatch, phoneWidth, wide) => {
+//   switch (mediaQueryMatch) {
+//     case "--fullWidth":
+//       return wide ? 860 : 760
+//     case "--tabletWidth":
+//       return 560
+//     case "--phoneWidth":
+//       return phoneWidth - 40
+//   }
+// }
 
 const ImagePlaceholder = ({ width, height, wide, children }) => {
-  const { mediaQueryMatch, phoneWidth } = useContext(MediaQueryContext)
+  // const { mediaQueryMatch, phoneWidth } = useContext(MediaQueryContext)
   const aspectRatio = height / width
+  const paddingBot = aspectRatio * 100
 
-  const getStyle = () => {
-    const containerWidth = getContainerWidth(mediaQueryMatch, phoneWidth, wide)
-    const finalWidth = Math.min(width, containerWidth)
-    const paddingBottom = aspectRatio * (finalWidth / containerWidth) * 100
+  // const getStyle = () => {
+  //   const containerWidth = getContainerWidth(mediaQueryMatch, phoneWidth, wide)
+  //   const finalWidth = Math.min(width, containerWidth)
+  //   const paddingBottom = aspectRatio * (finalWidth / containerWidth) * 100
 
-    return {
-      width: `${finalWidth}px`,
-      paddingBottom: `${paddingBottom}%`,
-    }
-  }
+  //   return {
+  //     width: `${finalWidth}px`,
+  //     paddingBottom: `${paddingBottom}%`,
+  //   }
+  // }
 
-  const [style, setStyle] = useState(getStyle())
+  // const [style, setStyle] = useState(getStyle())
 
-  useEffect(() => {
-    setStyle(getStyle())
-  }, [mediaQueryMatch, phoneWidth])
+  // useEffect(() => {
+  //   setStyle(getStyle())
+  // }, [mediaQueryMatch, phoneWidth])
 
   return (
     <div className={wide ? "outer-img-container wide" : "outer-img-container"}>
-      <div className="img-container" style={style}>
-        {children}
+      <div style={{ maxWidth: `${width}px`, margin: '0 auto' }}>
+        <div className="img-container" style={{ width: '100%', paddingBottom: `${paddingBot}%` }}>
+          {children}
+        </div>
       </div>
     </div>
   )
